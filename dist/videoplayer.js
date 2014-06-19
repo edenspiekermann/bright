51fb5bb9374ef38ecdb968696acd0a61cbd3da70
Maximilian Hoffmann <account@maximilianhoffmann.com>
2014-06-19 12:57:38 +0200

    remove non-standalone build versions

--- dist/videoplayer.js
+++ dist/videoplayer.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define(factory);
+	else if(typeof exports === 'object')
+		exports["videoplayer"] = factory();
+	else
+		root["videoplayer"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -1336,3 +1346,4 @@
 
 /***/ }
 /******/ ])
+})
