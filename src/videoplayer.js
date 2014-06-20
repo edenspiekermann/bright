e017e0b358965387fd2b7f8eda54e7a3366a8c48
Maximilian Hoffmann <account@maximilianhoffmann.com>
2014-06-20 12:36:53 +0200

    add object.create polyfill for IE8

--- src/videoplayer.js
+++ src/videoplayer.js
@@ -41,6 +41,25 @@ var playerPrototype = {
 
 };
 
+if (typeof Object.create != 'function') {
+  (function () {
+    var F = function () {};
+    Object.create = function (o) {
+      if (arguments.length > 1) {
+        throw Error('Second argument not supported');
+      }
+      if (o === null) {
+        throw Error('Cannot set a null [[Prototype]]');
+      }
+      if (typeof o != 'object') {
+        throw TypeError('Argument must be an object');
+      }
+      F.prototype = o;
+      return new F();
+    };
+  })();
+}
+
 function playerFactory() {
   var player = Object.create(playerPrototype);
   player = emitter(player);
